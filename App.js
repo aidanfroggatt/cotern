b91475ce2df32ee7329d3f497f6b29f61850a9b6
Aidan Froggatt <aidanfr@live.ca>
2024-02-09 15:12:57 -0500

    commented out all functionality to start firestore setup

--- App.js
+++ App.js
@@ -3,26 +3,35 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomePage from './components/pages/HomePage';
 import CommunitiesPage from './components/pages/CommunitiesPage';
 import AccountPage from './components/pages/AccountPage';
+import { View, Text } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
     return (
-        <NavigationContainer>
-            <Tab.Navigator initialRouteName="Home">
-                <Tab.Screen
-                    name="Home"
-                    component={HomePage}
-                />
-                <Tab.Screen
-                    name="Communities"
-                    component={CommunitiesPage}
-                />
-                <Tab.Screen
-                    name="Account"
-                    component={AccountPage}
-                />
-            </Tab.Navigator>
-        </NavigationContainer>
+        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+            <Text>Hi</Text>
+        </View>
     );
 }
+
+
+// Commented out naviagator to test firestore in App.js
+{/* 
+    <NavigationContainer>
+        <Tab.Navigator initialRouteName="Home">
+            <Tab.Screen
+                name="Home"
+                component={HomePage}
+            />
+            <Tab.Screen
+                name="Communities"
+                component={CommunitiesPage}
+            />
+            <Tab.Screen
+                name="Account"
+                component={AccountPage}
+            />
+        </Tab.Navigator>
+    </NavigationContainer> 
+*/}
