8c481371b555a9528d7362c390b5db90165f5ece
Aidan Froggatt <aidanfr@live.ca>
2024-02-10 22:22:43 -0500

    cleaning code

--- App.js
+++ App.js
@@ -1,9 +1,6 @@
 import { View, Text } from 'react-native';
-import { getUsers } from './controllers/usersController';
 
 export default function App() {
-
-    getUsers();
 
     return (
         <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
