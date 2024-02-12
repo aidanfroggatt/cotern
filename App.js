18fc21468116bb3f09953b15fad1c9e6442d1dee
Aidan Froggatt <aidanfr@live.ca>
2024-02-11 20:34:42 -0500

    created login page with auth

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native';
 import SignupPage from './pages/SignupPage';
+import LoginPage from './pages/LoginPage';
 
 export default function App() {
 
     return (
-        <SignupPage/>
+        <LoginPage/>
         // <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         //     <Text>Hi</Text>
         // </View>
