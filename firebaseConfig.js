f5c3d134e16e1d9ce36b0a8b47aff14304defcaf
Aidan Froggatt <aidanfr@live.ca>
2024-02-12 18:29:33 -0500

    firebase config environment variables didn't work

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,17 +4,16 @@ import { getAuth, initializeAuth } from "firebase/auth";
 import { getReactNativePersistence } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
-require('dotenv').config();
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyAos1eOmeDOE31XC4nPY8xXc0E9NeN5vTQ",
+    authDomain: "cotern.firebaseapp.com",
+    databaseURL: "https://cotern-default-rtdb.firebaseio.com",
+    projectId: "cotern",
+    storageBucket: "cotern.appspot.com",
+    messagingSenderId: "525666418897",
+    appId: "1:525666418897:web:ccc197c1cfeb4f659ac5f0",
+    measurementId: "G-LGZTJENBY6"
 };
 
 const app = initializeApp(firebaseConfig);
