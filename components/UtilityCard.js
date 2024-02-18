b1b485dcf3f6ff148bb7e414ceeb1e3d1d9c995f
Aidan Froggatt <aidanfr@live.ca>
2024-02-18 15:24:04 -0500

    updated utility card

--- components/UtilityCard.js
+++ components/UtilityCard.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { TouchableOpacity, View, Text } from 'react-native';
 
-const UtilityCard = ({ icon, title, children, onPress, disabled }) => {
+const UtilityCard = ({ icon, title, onPress, disabled }) => {
 
     return (
         <TouchableOpacity onPress={onPress} disabled={disabled}>
             <View className="flex-row justify-between items-center bg-gray-100 p-4 my-0.5 text-secondary rounded-2xl">
                 {icon}
                 <Text className="">{title}</Text>
-                {children}
             </View>
         </TouchableOpacity>
     );
