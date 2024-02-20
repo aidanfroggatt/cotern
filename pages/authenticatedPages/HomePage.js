91604e9e22b7560e0ed588c12abc75ae547816df
Aidan Froggatt <aidanfr@live.ca>
2024-02-19 23:21:08 -0500

    fixed image naming conventions, removed extra images

--- pages/authenticatedPages/HomePage.js
+++ pages/authenticatedPages/HomePage.js
@@ -5,7 +5,7 @@ const HomePage = () => {
 
 	return (
 		<SafeAreaView className="flex-1 justify-center items-center bg-primary">
-			<Image source={require("../../assets/illustrations/sweet-home_illustration.png")} style={{width:200, height:200}}/>
+			<Image source={require("../../assets/illustrations/sweet_home_illustration.png")} style={{width:200, height:200}}/>
 		</SafeAreaView>
 	);
 };
