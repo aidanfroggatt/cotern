0de1934513fc9734fa242e9bb48274d175d3cc4f
Aidan Froggatt <aidanfr@live.ca>
2024-02-21 17:51:01 -0500

    added tests for textlink component

---
+++ tests/components/TextLink.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react-native';
+import TextLink from './../../components/TextLink';
+
+describe('<TextLink />', () => {
+    test('renders correctly with provided title', () => {
+        const onPressMock = jest.fn();
+        const title = 'Click here';
+
+        const { getByText } = render(<TextLink onPress={onPressMock} title={title} />);
+
+        const link = getByText(title);
+        expect(link).toBeDefined();
+    });
+
+    test('calls onPress function when link is pressed', () => {
+        const onPressMock = jest.fn();
+        const title = 'Click here';
+
+        const { getByText } = render(<TextLink onPress={onPressMock} title={title} />);
+
+        const link = getByText(title);
+        fireEvent.press(link);
+
+        expect(onPressMock).toHaveBeenCalledTimes(1);
+    });
+});
