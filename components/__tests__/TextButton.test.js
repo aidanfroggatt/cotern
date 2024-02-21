c3ba11bd051d3843d45f445313585ad7aa564d76
Aidan Froggatt <aidanfr@live.ca>
2024-02-21 17:41:23 -0500

    created basic test for text button

---
+++ components/__tests__/TextButton.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react-native';
+import TextButton from './../TextButton';
+
+describe('<TextButton />', () => {
+    test('renders button with correct title', () => {
+        const onPressMock = jest.fn();
+        const title = 'Press me';
+
+        const { getByText } = render(<TextButton onPress={onPressMock} title={title} />);
+
+        const button = getByText(title);
+        expect(button).toBeDefined();
+    });
+
+    test('calls onPress function when button is pressed', () => {
+        const onPressMock = jest.fn();
+        const title = 'Press me';
+
+        const { getByText } = render(<TextButton onPress={onPressMock} title={title} />);
+
+        const button = getByText(title);
+        fireEvent.press(button);
+
+        expect(onPressMock).toHaveBeenCalledTimes(1);
+    });
+});
