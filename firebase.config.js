3d8d0f4e0522c68d7d56803f77dc1a0727ff1b3b
Aidan Froggatt <aidanfr@live.ca>
2024-02-22 21:49:34 -0500

    removed unnecessary exports

--- firebase.config.js
+++ firebase.config.js
@@ -13,7 +13,7 @@ import {
     FIREBASE_STORAGE_BUCKET
 } from "@env";
 
-export const firebaseConfig = {
+const firebaseConfig = {
     apiKey: FIREBASE_API_KEY,
     authDomain: FIREBASE_AUTH_DOMAIN,
     databaseURL: FIREBASE_DATABASE_URL,
@@ -24,7 +24,7 @@ export const firebaseConfig = {
     measurementId: FIREBASE_MEASUREMENT_ID
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 export const myAuth = initializeAuth(app, {
 	persistence: getReactNativePersistence(ReactNativeAsyncStorage)
